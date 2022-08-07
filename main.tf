1f1671ac902bf25e70af32d96a35e726abbd28d2
JackR42 <j.roumimper@gmail.com>
2022-08-07 23:21:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,35 @@ terraform {
 }
 
 ### BEGIN KeyVault
+data "azurerm_key_vault" "project" {
+  name                = 'core-project01-kv927432'
+  resource_group_name = "S2-RG-CORE-project01"
+}
+data "azurerm_key_vault_secret" "secret1" {
+  name         = "SqlServer-InstanceName"
+  key_vault_id = data.azurerm_key_vault.project42.id
+}
+data "azurerm_key_vault_secret" "secret2" {
+  name         = "SqlServer-InstanceAdminUserName"
+  key_vault_id = data.azurerm_key_vault.project42.id
+}
+data "azurerm_key_vault_secret" "secret3" {
+  name         = "SqlServer-InstanceAdminPassword"
+  key_vault_id = data.azurerm_key_vault.project42.id
+}
+
 ### END KeyVault
 
 ### BEGIN MAIN
-resource "azurerm_resource_group" "project01" {
+resource "azurerm_resource_group" "project" {
   name = "S2-RG-Project01"
   location = "westeurope"
+}
+resource "azurerm_mssql_server" "project" {
+ name  = data.azurerm_key_vault_secret.secret1.value
+ version = "12.0"
+ resource_group_name = azurerm_resource_group.project.name
+ location = azurerm_resource_group.project.location
+ administrator_login = data.azurerm_key_vault_secret.secret2.value
+ administrator_login_password = data.azurerm_key_vault_secret.secret3.value
 }
